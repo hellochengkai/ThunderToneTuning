cb28e19311efab939968a392e3f5345dd2e8d232
成凯 <hellochengkai@hotmail.com>
2018-10-15 11:20:21 +0800

    add music fragment

--- app/src/main/java/com/thunder/ktv/thundertonetuning/MainActivity.java
+++ app/src/main/java/com/thunder/ktv/thundertonetuning/MainActivity.java
@@ -1,13 +1,22 @@
 package com.thunder.ktv.thundertonetuning;
 
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    FragmentManager fragmentManager;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        fragmentManager = getSupportFragmentManager();
+
+        Fragment musicFragment = fragmentManager.findFragmentById(R.id.fragment_container);
+        if(musicFragment == null){
+            fragmentManager.beginTransaction().add(R.id.fragment_container,new MusicFragment()).commit();
+        }
     }
 }
---
+++ app/src/main/java/com/thunder/ktv/thundertonetuning/MusicFragment.java
@@ -0,0 +1,36 @@
+package com.thunder.ktv.thundertonetuning;
+
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+/**
+ * Created by chengkai on 18-10-15.
+ */
+
+public class MusicFragment extends Fragment {
+    public void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+    }
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    private TextView textView;
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_music,container,false);
+        textView = view.findViewById(R.id.music_title);
+//        textView.setBackgroundColor(getResources().getColor(R.color.colorPrimary));
+        return view;
+    }
+}
